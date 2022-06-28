370978fd7978cb951bfc261c1728975714981c94
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-06-29 00:55:31 +0700

    Implement Model Validation

--- MusicAPI/Models/Song.cs
+++ MusicAPI/Models/Song.cs
@@ -1,10 +1,15 @@
-﻿namespace MusicAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicAPI.Models
 {
     public class Song
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Judul tidak boleh kosong")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Bahasa tidak boleh kosong")]
         public string Language { get; set; }
+        [Required(ErrorMessage = "Mohon untuk mengisi durasi lagu")]
         public string Duration { get; set; }
     }
 }
