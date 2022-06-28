e7d850ccc39223ca0deef108cd85bf0ceee2598c
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-06-28 21:26:06 +0700

    Return List of Data

---
+++ MusicAPI/Controllers/SongsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Models;
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private List<Song> songs = new List<Song>()
+        {
+            new Song(){Id =0, Title = "Masih", Language="Indonesia"},
+            new Song(){Id =1, Title = "Setia", Language="Indonesia"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+    }
+}
